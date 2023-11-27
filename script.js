67a784f3495b91ed9f302d6436ea91ca6403b482
Kash <141306723+kxxg1@users.noreply.github.com>
2023-11-28 00:19:48 +1100

    style.css created, minor changes to .html and .js

--- script.js
+++ script.js
@@ -1,17 +1,62 @@
 // CHECKPOINT 1
+// Refactored WeatherChecker promise
+function WeatherChecker(forecast, partyDate, userName) {
+  return new Promise((resolve, reject) => {
+      const friends = forecast === 'forecast is sunny' ? 20 : 10;
+      if (forecast === 'forecast is sunny') {
+          resolve(`${partyDate} - ${userName}, Sunny weather, enjoy picnic party, invite ${friends} guests`);
+      } else {
+          reject(`${partyDate} - ${userName}, Rainy weather, enjoy house party, invite ${friends} guests`);
+      }
+  });
+}
 
-const WeatherChecker = new Promise((resolve, reject) => {
-    const isWeather = true;
-    let friends = 20
-    if (isWeather) {
-        resolve(`Party in the Park with ${friends} guests.`)
-    }else {
-        friends = friends -10
-        reject(`Party at home with ${friends} guests.`)
-    }
-        
-  })
+// Event listener for the user form
+document.querySelector('.user-form').addEventListener('submit', async function (e) {
+  e.preventDefault(); // Prevent the form from submitting the traditional way
 
+  const userName = document.getElementById('user-name').value;
+  const partyDate = document.getElementById('party-date').value;
+  const resultList = document.getElementById('result-list');
+
+  try {
+      const forecast = await randomForecast();
+
+      WeatherChecker(forecast, partyDate, userName)
+          .then(result => {
+              console.log(result);
+              const listItem = document.createElement('li');
+              listItem.textContent = result;
+              resultList.appendChild(listItem);
+          })
+          .catch(error => {
+              console.log(error);
+              const listItem = document.createElement('li');
+              listItem.textContent = error;
+              resultList.appendChild(listItem);
+          });
+  } catch (error) {
+      console.error("Error fetching the forecast: ", error);
+  }
+});
+
+// The randomForecast function
+function randomForecast() {
+  return new Promise((resolve) => {
+      setTimeout(() => {
+          const isSunny = Math.random() < 0.5;
+          resolve(isSunny ? 'forecast is sunny' : 'forecast is rainy');
+      }, 1000); // 1 second delay to simulate an API call
+  });
+}
+
+
+
+
+
+
+
+/*
 //   CHECKPOINT 2
 
 const getData = async () => {
@@ -32,7 +77,7 @@ const getData = async () => {
   }
 
   // Checkpoint 3
-
+*/
   
 
 
